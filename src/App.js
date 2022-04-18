257a061de38df02622c350f0e28a4f9a0352d173
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2022-04-18 17:49:40 +0600

    Home page contents

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
@@ -17,6 +18,7 @@ function App() {
                 <Route path='/login' element={<Login></Login>}></Route>
                 <Route path='/register' element={<Register></Register>}></Route>
             </Routes>
+            <Footer></Footer>
         </div>
     );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className='py-5 text-white'>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-4">
+                        <div className="brand">
+                            Doctor Sakib
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <Link to="/">Home</Link>
+                        <Link to="/">Home</Link>
+                        <Link to="/">Home</Link>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="social-links">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
